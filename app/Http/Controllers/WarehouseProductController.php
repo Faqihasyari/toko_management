2432241ede00ed516c01faa3ab88264a62f33894
Faqihasyari <faqih8158@gmail.com>
2025-09-01 21:34:58 +0700

    push

--- app/Http/Controllers/WarehouseProductController.php
+++ app/Http/Controllers/WarehouseProductController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\WarehouseProductRequest;
+use App\Services\WarehouseService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class WarehouseProductController
 {
     //
+
+    private WarehouseService $warehouseService;
+
+    public function __construct(WarehouseService $warehouseService)
+    {
+        $this->warehouseService = $warehouseService;
+    }
+
+    public function attach(WarehouseProductRequest $request, int $warehouseId): JsonResponse
+    {
+
+
+        $this->warehouseService->attachProduct(
+            $warehouseId,
+            $request->input('product_id'),
+            $request->input('stock')
+        );
+
+        return response()->json(['message' => 'Product attached successfully']);
+    }
 }
