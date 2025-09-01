c0d425d7940156dcb84bd0e7b53caaab52824712
Faqihasyari <faqih8158@gmail.com>
2025-09-01 23:31:49 +0700

    push

--- app/Http/Controllers/MerchantController.php
+++ app/Http/Controllers/MerchantController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\MerchantService;
 use Illuminate\Http\Request;
 
 class MerchantController
 {
     //
-}
+
+    private MerchantService $merchantService;
+
+    public function __construct(MerchantService $merchantService)
+    {
+        $this->merchantService = $merchantService;
+    }
+} 
