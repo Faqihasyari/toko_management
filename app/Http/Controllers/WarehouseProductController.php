467d2aa823088ff02b5d4589698ee27d12b09bc8
Faqihasyari <faqih8158@gmail.com>
2025-09-01 21:51:31 +0700

    push

--- app/Http/Controllers/WarehouseProductController.php
+++ app/Http/Controllers/WarehouseProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\WarehouseProductRequest;
+use App\Http\Requests\WarehouseProductUpdateRequest;
 use App\Services\WarehouseService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -29,5 +30,25 @@ class WarehouseProductController
         );
 
         return response()->json(['message' => 'Product attached successfully']);
+    }
+
+    public function detach(int $warehouseId, int $productId)
+    {
+        $this->warehouseService->detachProduct($warehouseId, $productId);
+        return response()->json(['message' => 'Product detached successfully']);
+    }
+
+    public function update(WarehouseProductUpdateRequest $request, int $warehouseId, int $productId)
+    {
+        $warehouseProduct = $this->warehouseService->updateProductStock(
+            $warehouseId,
+            $productId,
+            $request->validated()['stock']
+        );
+
+        return response()->json([
+            'message' => 'Stock updated successfully',
+            'data' => $warehouseProduct,
+        ]);
     }
 }
