cd5785e4136a7a29750312fc16f461b0fef1d609
Faqihasyari <faqih8158@gmail.com>
2025-09-06 21:33:39 +0700

    injeksion

--- app/Http/Controllers/MerchantProductController.php
+++ app/Http/Controllers/MerchantProductController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\MerchantProductService;
 use Illuminate\Http\Request;
 
 class MerchantProductController
 {
     //
+
+    private MerchantProductService $merchantProductService;
+
+    public function __construct(MerchantProductService $merchantProductService)
+    {
+        $this->merchantProductService = $merchantProductService;
+    }
 }
