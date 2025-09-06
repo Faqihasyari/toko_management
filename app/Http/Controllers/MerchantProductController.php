98f5edb48091b409adc9e19d3807dbbc658f85b0
Faqihasyari <faqih8158@gmail.com>
2025-09-06 21:41:04 +0700

    push

--- app/Http/Controllers/MerchantProductController.php
+++ app/Http/Controllers/MerchantProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\MerchantProductRequest;
 use App\Services\MerchantProductService;
 use Illuminate\Http\Request;
 
@@ -14,5 +15,18 @@ class MerchantProductController
     public function __construct(MerchantProductService $merchantProductService)
     {
         $this->merchantProductService = $merchantProductService;
+    }
+
+    public function store(MerchantProductRequest $request, int $merchant)
+    {
+        $validated = $request->validated();
+        $validated['merchant_id'] = $merchant;
+
+        $merchantProduct = $this->merchantProductService->assignProductToMerchant($validated);
+
+        return response()->json([
+            'message' => 'Product assigned to merchant successfully',
+            'data' => $merchantProduct,
+        ], 201);
     }
 }
