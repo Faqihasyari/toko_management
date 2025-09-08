7202dbd8e967445cc862ac2b1465b908c6147a3d
Faqihasyari <faqih8158@gmail.com>
2025-09-08 14:51:16 +0700

    push

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProductRequest;
+use App\Http\Resources\ProductResource;
+use App\Services\ProductService;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
 class ProductController
 {
     //
+
+    private ProductService $productService;
+
+    public function __construct(ProductService $productService)
+    {
+        $this->productService = $productService;
+    }
+
+    public function index()
+    {
+        $fields = ['id', 'name', 'thumbnail', 'price', 'category_id'];
+        $products = $this->productService->getAll($fields);
+        return response()->json(ProductResource::collection($products));
+    }
+
+    public function show(int $id)
+    {
+        try {
+            $fields = ['id', 'name', 'thumbnail', 'price', 'about', 'category_id'];
+            $product = $this->productService->getById($id, $fields);
+            return response()->json(new ProductResource($product));
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'message' => 'product not found',
+            ], 404);
+        }
+    }
+
+    public function store(ProductRequest $request)
+    {
+        $product = $this->productService->create($request->validated());
+        return response()->json(new ProductResource($product), 201);
+    }
 }
