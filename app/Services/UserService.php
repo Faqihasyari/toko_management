a7083b8892bf68ca7c3d533cc1bc0f955d690322
Faqihasyari <faqih8158@gmail.com>
2025-09-08 21:46:47 +0700

    push

---
+++ app/Services/UserService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\UserRepository;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Storage;
+
+class UserService
+{
+    private UserRepository $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    public function getAll(array $fields)
+    {
+        return $this->userRepository->getAll($fields);
+    }
+
+    public function getById(int $id, array $fields)
+    {
+        return $this->userRepository->getById($id, $fields ?? ['*']);
+    }
+
+    private function uploadPhoto(UploadedFile $photo)
+    {
+        return $photo->store('users', 'public');
+    }
+
+    private function deletePhoto(string $photoPath)
+    {
+        $relativePath = 'users/' . basename($photoPath);
+        if (Storage::disk('public')->exists($relativePath)) {
+            Storage::disk('public')->delete($relativePath);
+        }
+    }
+}
