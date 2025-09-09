635f8319cb1feea231d5b372d0cef5290397ff86
Faqihasyari <faqih8158@gmail.com>
2025-09-09 22:29:10 +0700

    push

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\UserResource;
 use App\Services\UserService;
 use Illuminate\Http\Request;
 
@@ -14,5 +15,19 @@ class UserController extends Controller
     public function __construct(UserService $userService)
     {
         $this->userService = $userService;
+    }
+
+    public function index()
+    {
+        $fields = ['id', 'name', 'email', 'photo', 'phone'];
+        $users = $this->userService->getAll($fields ?: ['*']);
+        return response()->json(UserResource::collection($users));
+    }
+
+    public function show(int $id)
+    {
+        $fields = ['id', 'name', 'email', 'photo', 'phone'];
+        $user = $this->userService->getById($id, $fields ?: ['*']);
+        return response()->json(new UserResource($user));
     }
 }
