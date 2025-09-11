51947b9c11407436e6cf74d1356db60ee64e9ed6
Faqihasyari <faqih8158@gmail.com>
2025-09-11 21:52:38 +0700

    push

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\RoleResource;
+use App\Repositories\RoleService;
 use Illuminate\Http\Request;
 
 class RoleController extends Controller
 {
     //
+
+    private RoleService $roleService;
+
+    public function __construct(RoleService $roleService)
+    {
+        $this->roleService = $roleService;
+    }
+
+    public function index()
+    {
+        $fields = ['id', 'name'];
+        $roles = $this->roleService->getAll($fields ?: ['*']);
+        return response()->json(RoleResource::collection($roles));
+    }
+
+    public function show(int $id)
+    {
+        $fields = ['id', 'name'];
+        $role = $this->roleService->getById($id, $fields ?: ['*']);
+        return response()->json(new RoleResource($role));
+    }
 }
