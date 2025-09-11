93e9ff476822367815c1079849919ec782191c5c
Faqihasyari <faqih8158@gmail.com>
2025-09-11 21:56:51 +0700

    push

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RoleRequest;
 use App\Http\Resources\RoleResource;
 use App\Repositories\RoleService;
 use Illuminate\Http\Request;
@@ -20,7 +21,7 @@ class RoleController extends Controller
     public function index()
     {
         $fields = ['id', 'name'];
-        $roles = $this->roleService->getAll($fields ?: ['*']);
+        $roles = $this->roleService->getAll($fields);
         return response()->json(RoleResource::collection($roles));
     }
 
@@ -29,5 +30,23 @@ class RoleController extends Controller
         $fields = ['id', 'name'];
         $role = $this->roleService->getById($id, $fields ?: ['*']);
         return response()->json(new RoleResource($role));
+    }
+
+    public function store(RoleRequest $request)
+    {
+        $role = $this->roleService->create($request->validated());
+        return response()->json(new RoleResource($role), 201);
+    }
+
+    public function update(RoleRequest $request, int $id)
+    {
+        $role = $this->roleService->update($id, $request->validated());
+        return response()->json(new RoleResource($role));
+    }
+
+    public function destroy(int $id)
+    {
+        $this->roleService->delete($id);
+        return response()->json(['message' => 'Role deleted successfully']);
     }
 }
