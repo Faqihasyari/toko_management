e9a3393b4f62f7f30ab68d65543f563ba237a488
Faqihasyari <faqih8158@gmail.com>
2025-09-11 21:43:56 +0700

    push

--- app/Services/RoleService.php
+++ app/Services/RoleService.php
@@ -4,5 +4,20 @@ namespace App\Repositories;
 
 class RoleService
 {
-    
+    private RoleRepository $roleRepository;
+
+    public function __construct(RoleRepository $roleRepository)
+    {
+        $this->roleRepository = $roleRepository;
+    }
+
+    public function getAll(array $fields)
+    {
+        return $this->roleRepository->getAll($fields);
+    }
+
+    public function getById(int $id, array $fields)
+    {
+        return $this->roleRepository->getById($id, $fields ?? ['*']);
+    }
 }
