629fc8965aa1a16666e7ce179712738ef0418ce0
Faqihasyari <faqih8158@gmail.com>
2025-09-16 21:29:01 +0700

    push

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\TransactionResource;
+use App\Services\TransactionService;
 use Illuminate\Http\Request;
 
 class TransactionController
 {
     //
+
+    private TransactionService $transactionService;
+
+    public function __construct(TransactionService $transactionService)
+    {
+        $this->transactionService = $transactionService;
+    }
+
+    public function index()
+    {
+        $fields = ['*'];
+        $transactions = $this->transactionService->getAll($fields);
+        return response()->json(TransactionResource::collection($transactions));
+    }
 }
