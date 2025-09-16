e941039638b79508511c2472636376535146f372
Faqihasyari <faqih8158@gmail.com>
2025-09-16 15:46:28 +0700

    push

--- app/Services/TransactionService.php
+++ app/Services/TransactionService.php
@@ -5,6 +5,9 @@ namespace App\Services;
 use App\Repositories\MerchantProductRepository;
 use App\Repositories\ProductRepository;
 use App\Repositories\TransactionRepositor;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\ValidationException;
 use MerchantRepository;
 
 class TransactionService
@@ -29,5 +32,27 @@ class TransactionService
     public function getAll(array $fields)
     {
         return $this->transactionRepository->getAll($fields);
+    }
+
+    public function createTransaction(array $data)
+    {
+        return DB::transaction(function () use ($data) {
+            $merchant = $this->merchantRepository->getById($data['merchant_id'], ['id', 'keeper_id']);
+
+            if (!$merchant) {
+                throw ValidationException::withMessages([
+                    'merchant_id' => ['Merchant not found.']
+                ]);
+            }
+
+            if (Auth::id() !== $merchant->keeper_id) {
+                throw ValidationException::withMessages([
+                    'authorization' => ['Unauthorized: You can only process transactions for your assigned merchant.']
+                ]);
+            }
+
+            $products = [];
+            $subTotal = 0;
+        });
     }
 }
