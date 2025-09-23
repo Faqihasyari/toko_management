97867e9267a3487522955c48d1160b0bda2ed207
Faqihasyari <faqih8158@gmail.com>
2025-09-23 22:43:04 +0700

    fix db

--- database/migrations/2025_08_29_144412_create_merchants_table.php
+++ database/migrations/2025_08_29_144412_create_merchants_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
     {
         Schema::create('merchants', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
+            $table->string('name');
             $table->text('address');
             $table->string('photo');
-            $table->string('phone')->unique();
+            $table->string('phone');
             $table->foreignId('keeper_id')->constrained('users')->onDelete('cascade');
             $table->softDeletes();
             $table->timestamps();
